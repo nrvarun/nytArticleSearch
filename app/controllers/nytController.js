d81f7417637a9ead286dab44c070be3f004744f4
nrvarun <nr.varun79@gmail.com>
2016-06-13 09:22:57 +0530

    NYT API integrated
    The API integration has been done with keeping in mind all the statuses and also the error messages will also be shown if any error occurs.

--- app/controllers/nytController.js
+++ app/controllers/nytController.js
@@ -1,11 +1,12 @@
 angular.module('nytSearchApp').controller('nytCtrl', function ($scope,$resource) {
 	$scope.appName = 'New york Times App';
+	$scope.search_city = '';
+	$scope.error_status = true;
+	$scope.error_message = 'Unable to load New York times API Data :( ';
 
-	$scope.getArticles = function(){
+	$scope.getArticles = function(city){
 
 			var API_KEY = '4eab7fb120de4ef2b07371404a9d2257';
-
-			var city = $scope.search_city;
 
 			var articles = $resource(myApp.API_PATH+'?api-key='+API_KEY+'&fq='+city);
 
@@ -13,11 +14,29 @@ angular.module('nytSearchApp').controller('nytCtrl', function ($scope,$resource)
 			  console.log('success');
 			  console.log(success.response.docs);
 			  $scope.articles = success.response.docs;
+			  $scope.error_status = false;
 			},
 				function(error){
 					console.log('error');
+					console.log(error);
+					if(error.statusText === ''){
+						alert('Error 429 API Limit exceeded');
+					}
 				}
 			);
 	};
-	
+
+	document.getElementById('search-nyt-btn').addEventListener('click', function(){
+		var city = document.getElementById('search-city').value;
+		console.log(city);
+
+		if(city === undefined || city == '')	{
+			alert('enter a city value to get articles of your city');
+		}
+		else {
+			$scope.getArticles(city);
+		}
+
+	});
+
 });
